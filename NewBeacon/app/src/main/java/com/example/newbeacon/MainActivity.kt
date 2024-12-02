cfd4290306ebc6e42c614ef0b4b002b372627415
Raghav Kumar <raghavcast@gmail.com>
2024-12-02 13:02:05 +0530

    Advertising now stops in TestBeacon

--- NewBeacon/app/src/main/java/com/example/newbeacon/MainActivity.kt
+++ NewBeacon/app/src/main/java/com/example/newbeacon/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : ComponentActivity() {
     }
     private val requiredPermissions = arrayOf(
         android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.BLUETOOTH_ADMIN,
         android.Manifest.permission.BLUETOOTH_ADVERTISE,
         android.Manifest.permission.BLUETOOTH_SCAN,
         android.Manifest.permission.BLUETOOTH_CONNECT
--- TestBeacon/app/src/main/java/com/example/testbeacon/MainActivity.kt
+++ TestBeacon/app/src/main/java/com/example/testbeacon/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.testbeacon
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.AdvertiseCallback
 import android.bluetooth.le.AdvertiseData
 import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.AdvertisingSet
 import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var radioGroup: RadioGroup
     private lateinit var radioButton: RadioButton
     private lateinit var bluetoothLeAdvertiser: BluetoothLeAdvertiser
+    private lateinit var bluetoothAdapter: BluetoothAdapter
 
     private var isAdvertising = false
 
@@ -34,23 +39,28 @@ class MainActivity : AppCompatActivity() {
     private val busHash: HashMap<String, String> =
         HashMap()
 
-    private val advertiseCallback = object : AdvertiseCallback() {
-        override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
-            Log.i("BLE", "Advertising started successfully")
-        }
+    private val settings = AdvertiseSettings.Builder()
+        .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+        .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+        .setConnectable(false)
+        .build()
 
-        override fun onStartFailure(errorCode: Int) {
-            Log.e("BLE", "Advertising failed with error code: $errorCode")
-        }
-    }
+    private var advertiseCallback: AdvertiseCallback? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         supportActionBar!!.hide()
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothLeAdvertiser = bluetoothManager.adapter.bluetoothLeAdvertiser
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        bluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
+
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+            Log.e("iBeacon", "Bluetooth is off or not supported; cannot stop advertising")
+            return
+        }
 
         busHash["TN 015 0123"] = "27B2F751-228D-4BEA-8A06-F8ADC74388E6".lowercase()
         busHash["KA 321 3210"] = "ACA22A9D-06B2-4789-9669-9313B2F5605A".lowercase()
@@ -85,6 +95,7 @@ class MainActivity : AppCompatActivity() {
 
         stopButton.setOnClickListener {
             if (isAdvertising) {
+//                Log.i("Callback", "Reacting to button")
                 stopBeacon()
             }
         }
@@ -96,16 +107,27 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        if (!bluetoothAdapter.isEnabled) {
+            Log.e("iBeacon", "Bluetooth is off or not supported; cannot stop advertising")
+            return
+        }
+
+        if (advertiseCallback == null) {
+            advertiseCallback = object : AdvertiseCallback() {
+                override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
+                    Log.i("Callback", "Advertising started successfully")
+                }
+
+                override fun onStartFailure(errorCode: Int) {
+                    Log.e("Callback", "Advertising failed with error code: $errorCode")
+                }
+            }
+        }
+
 //        if (bluetoothLeAdvertiser == null) {
 //            Log.e("BLEBeacon", "BLE Advertising is not supported on this device.")
 //            return
 //        }
-
-        val settings = AdvertiseSettings.Builder()
-            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-            .setConnectable(false)
-            .build()
 
         val checkedButton = radioGroup.checkedRadioButtonId
         if (checkedButton == -1) {
@@ -131,13 +153,13 @@ class MainActivity : AppCompatActivity() {
             .addManufacturerData(0x4C00, beaconData) // 0x4C00 is Apple's Manufacturer ID
             .build()
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_ADVERTISE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions()
-        }
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.BLUETOOTH_ADVERTISE
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            requestPermissions()
+//        }
 
         try {
             bluetoothLeAdvertiser.startAdvertising(settings, data, advertiseCallback)
@@ -154,13 +176,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun stopBeacon() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_ADVERTISE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions()
+        if (advertiseCallback == null) {
+            Log.e("Callback", "AdvertiseCallback is null; cannot stop advertising")
+            return
         }
+
+        if (!bluetoothAdapter.isEnabled) {
+            Log.e("iBeacon", "Bluetooth is off or not supported; cannot stop advertising")
+            return
+        }
+
+        if(!hasPermissions()) {
+            Log.e("BLEBeacon", "Required permissions are not granted")
+            return
+        }
+
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.BLUETOOTH_ADVERTISE
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            requestPermissions()
+//        }
 
         try {
             bluetoothLeAdvertiser.stopAdvertising(advertiseCallback)
@@ -197,15 +234,24 @@ class MainActivity : AppCompatActivity() {
             arrayOf(
                 Manifest.permission.BLUETOOTH_ADVERTISE,
                 Manifest.permission.BLUETOOTH_SCAN,
-                Manifest.permission.BLUETOOTH_CONNECT
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADMIN
             )
         } else {
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            arrayOf(
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
         }
 
         return permissions.all {
             ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
         }
+    }
+
+    private fun promptEnableBluetooth() {
+        Toast.makeText(this, "Please enable bluetooth to start advertising", Toast.LENGTH_LONG).show()
+        startActivity(Intent(Settings.ACTION_BLUETOOTH_SETTINGS))
     }
 
     private fun requestPermissions() {
@@ -216,7 +262,10 @@ class MainActivity : AppCompatActivity() {
                 Manifest.permission.BLUETOOTH_CONNECT
             )
         } else {
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            arrayOf(
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
         }
 
         ActivityCompat.requestPermissions(this, permissions, 1)
@@ -228,8 +277,8 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(hasPermissions()) {
-            startBeacon()
-        }
+//        if(hasPermissions()) {
+//            startBeacon()
+//        }
     }
 }
